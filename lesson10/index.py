3b8beb37768e99139c5ce6e70406422f86b27af5
Bowei0204 <101374103+Bowei0204@users.noreply.github.com>
2024-07-19 08:04:48 +0000

    1

--- lesson10/index.py
+++ lesson10/index.py
@@ -3,11 +3,16 @@ import learning
 def main():
     p1 = learning.getPerson(name="1")
     print(p1.B_print())
-    
+
     print("==================")
 
     p2 = learning.Person.getPerson(name="2")
     print(p2.B_print())
+
+    print("==================")
+
+    p3 = learning.Person.getPerson1(name="3")
+    print(p3.B_print())
 
 if __name__ == '__main__':
     main()
